406d98cb2e26bce3d5c638a1de604eff85f648dd
Ksenia1290 <Ksu.tender@yandex.ru>
2024-06-13 23:27:38 +0300

    1

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -42,10 +42,10 @@ function Affair(props: AffairPropsType) {
             </div>
 
             <button
-                id={'hw2-button-delete-' + props.affair._id}
-                className={buttonClass}
+                //id={'hw2-button-delete-' + props.affair._id}
+               // className={buttonClass}
                 // need to fix
-                onClick={deleteCallback}//ОНКЛИК={ФУНКЦИЯ}
+                onClick={deleteCallback} className={s.item+''+ s.button}//ОНКЛИК={ФУНКЦИЯ}
             >
                 {/*текст кнопки могут изменить студенты*/}
                 X
