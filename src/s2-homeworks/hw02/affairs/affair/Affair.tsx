d83469b8d2f82563c3763cc756a9e81286c8e277
Ksenia1290 <Ksu.tender@yandex.ru>
2024-06-13 23:06:21 +0300

    1

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -10,7 +10,7 @@ type AffairPropsType = {
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {
+    const deleteCallback = () => {props.deleteAffairCallback(props.affair._id)}
         // need to fix
         // пропс.функция(мне нужен _id)
         // давайте проследим боевой путь это функции, или как она будет всплывать:
@@ -19,7 +19,7 @@ function Affair(props: AffairPropsType) {
         // далее из Affairs всплывет в HW2->
         // в HW2 находим deleteAffairCallback- это и есть наш клиент ->
         // deleteAffairCallback вызовет setAffairs(...) и   deleteAffair(...)
-    }
+    
 
     const nameClass = s.name + ' ' + s2[props.affair.priority]
     const buttonClass = s.closeButton + ' ' + s2[props.affair.priority]
@@ -32,12 +32,12 @@ function Affair(props: AffairPropsType) {
         >
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
                 {/*создаёт студент*/}
-                {/* ПРОПС.ВЫВОДИМ ИМЯ*/}
+                {props.affair.name}{/* ПРОПС.ВЫВОДИМ ИМЯ*/}
                 {/**/}
             </div>
             <div id={'hw2-priority-' + props.affair._id} hidden>
                 {/*создаёт студент*/}
-
+                 {props.affair.priority}
                 {/**/}
             </div>
 
@@ -45,7 +45,7 @@ function Affair(props: AffairPropsType) {
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
                 // need to fix
-                //ОНКЛИК={ФУНКЦИЯ}
+                onClick={deleteCallback}//ОНКЛИК={ФУНКЦИЯ}
             >
                 {/*текст кнопки могут изменить студенты*/}
                 X
